bdb10455f26509388cac08c806dcfa28b60db910
carlatv <carla.ten@upf.edu>
2018-05-22 16:01:49 +0200

    parses the tree correctly. pending: separate arguments correctly

--- Scripts/parse_lex.py
+++ Scripts/parse_lex.py
@@ -10,39 +10,75 @@ root = parsing.getroot()
 class WrapFeatures(object):
     def __init__(self):
         self.attrs = {}
+        # self.otherstuff = otherstuff
     def __str__(self):
         output = "{"
         for attr in self.attrs:
             output += "%s = %s\n" %(attr, self.attrs.get(attr))
+        # output += "%s" %self.otherstuff
         output += "}"
         return output
 
-wrapping = WrapFeatures()
+class GetTheMiddle(object):
+    def __init__(self):
+        self.attrs = {}
+    def __str__(self):
+        output = ""
+        for attr in self.attrs:
+            output += "\t%s = %s\n" %(attr, self.attrs.get(attr))
+        return output
+
+
 
 names = ['anc_sense', 'lss', 'type', 'argument', 'thematicrole', 'function', 'constituent', 'preposition']
-
+result = []
 
 def ParseFile(root):
-    for sense in root.findall('sense'):
-        id = sense.get('id')
-        # anc_sense = ('anc_sense  = ' + id)
-        wrapping.attrs = {names[0]:id}
-        for frame in sense.iter('frame'):
-            # anc_lss.append(frame.get('lss'))
-            lss = frame.get('lss')
-            type = frame.get('type')
-            {names[1]: lss, names[2]: type}
-            wrapping.attrs.update({names[1]:lss, names[2]:type})
+        for sense in root.findall('sense'):
+            id = sense.get('id')
+            # anc_sense = ('anc_sense  = ' + id)
+            wrapping = WrapFeatures()
+            wrapping.attrs = {names[0]:id}
+            for frame in sense.iter('frame'):
+                # anc_lss.append(frame.get('lss'))
+                lss = frame.get('lss')
+                type = frame.get('type')
+                wrapping.attrs.update({names[1]:lss, names[2]:type})
+                result.append(str(wrapping))
+        # print("result inside function")
+        # print(result)
+        return result
 
-            for argument in frame.iter('argument'):
-                arg = argument.get('argument')
-                themrole = argument.get('thematicrole')
-                funct = argument.get('function')
 
-                wrapping.attrs.update({names[3]:arg, names[4]:themrole, names[5]:funct})
-                print wrapping
+res = ParseFile(root)
+middle = GetTheMiddle()
+mid_res = []
+def Arguments(root):
+    for arguments in root.iter('argument'):
+        arg = arguments.get('argument')
+        themrole = arguments.get('thematicrole')
+        funct = arguments.get('function')
 
-    return wrapping
+        middle.attrs = {names[3]:arg, names[4]:themrole, names[5]:funct}
+        mid_res.append(str(middle))
 
-ParseFile(root)
+    return mid_res
+
+
+# coses = ['casa','cosa']
+mid_res = Arguments(root)
+#
+# for j in range(0, len(mid_res)):
+#     print mid_res[j]
+
+# print mid_res
+
+for j in range(0, len(res)):
+    total  = res[j]
+    for i in range(0, len(mid_res)):
+        total += mid_res[i]
+        total += "\n"
+    print total
+# print "result outside function:"
+# print res[0],"\n", res[1]
 # print str(result)
