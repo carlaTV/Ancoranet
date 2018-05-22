8919ae01f7d1e435f2863336af2e1ae8ccf1ccef
carlatv <carla.ten@upf.edu>
2018-05-22 11:00:12 +0200

    save properties using a dictionary

--- Scripts/matedict.py
+++ Scripts/matedict.py
@@ -10,12 +10,12 @@ class Entry(object):
     def __init__(self, name, parent):
         self.name = name
         self.parent = parent
-        self.attrs = []
+        self.attrs = {}
 
     def __str__(self):
         output = "%s:%s {\n" % (self.name, self.parent)
         for attr in self.attrs:
-            output += "%s\n" % attr
+            output += "%s = %s\n" % (self.attrs.get(attr), attr)
         output += "\n}"
         return output.encode("utf8")
 
@@ -37,30 +37,25 @@ ancoranet_es = ET.parse(codecs.open('../OriginalFiles/ancoranet-es.xml', encodin
 # generem la root:
 root_ancoranet = ancoranet_es.getroot()
 
-verbs = []
+names = ['anc_vtype', 'pbcls','pbID']
 
 with codecs.open('../OutputFiles/dictionary.txt','w') as fd:
 
     for link in root_ancoranet:
         lexid = link.get('ancoralexid')
         VB, name_verb, num, anc_vtype = lexid.split('.')
-        anc_vtype = ('anc_vtype = ' + anc_vtype)
+        # anc_vtype = ('anc_vtype = ' + anc_vtype)
         name = (VB + '_' + name_verb + '_' + '0' + num)
         spec = ('_' + VB + '_')
 
         bankID = link.get('propbankid')
         pbcls, pbID = bankID.split('.')
 
-        pbcls = ('pbcls =' + pbcls)
-        pbID = ('pbID = ' + pbID)
 
-        AncoranetEntry = Entry(name, spec)
-        AncoranetEntry.attrs.append(anc_vtype)
-        AncoranetEntry.attrs.append(pbcls)
-        AncoranetEntry.attrs.append(pbID)
+        ancoranet_entry = Entry(name, spec)
+        ancoranet_entry.attrs = {anc_vtype: names[0], pbcls : names[1], pbID: names[2]}
 
-
-        fd.write(str(AncoranetEntry))
-        # print(str(AncoranetEntry).encode('Unicode'))
+        fd.write(str(ancoranet_entry))
+        # print(AncoranetEntry)
 
 fd.close()
