0e80b29ec7acc105649eb603c8f53b44bd4086a7
carlatv <carla.ten@upf.edu>
2018-05-22 16:35:34 +0200

    parses until "frame" saves the result in a list, which can be called element by element to retrieve each sense of a verb

--- Scripts/parse_lex.py
+++ Scripts/parse_lex.py
@@ -19,21 +19,10 @@ class WrapFeatures(object):
         output += "}"
         return output
 
-class GetTheMiddle(object):
-    def __init__(self):
-        self.attrs = {}
-    def __str__(self):
-        output = ""
-        for attr in self.attrs:
-            output += "\t%s = %s\n" %(attr, self.attrs.get(attr))
-        return output
-
-
-
-names = ['anc_sense', 'lss', 'type', 'argument', 'thematicrole', 'function', 'constituent', 'preposition']
-result = []
+names = ['anc_sense', 'lss', 'type']
 
 def ParseFile(root):
+        res = []
         for sense in root.findall('sense'):
             id = sense.get('id')
             # anc_sense = ('anc_sense  = ' + id)
@@ -44,41 +33,8 @@ def ParseFile(root):
                 lss = frame.get('lss')
                 type = frame.get('type')
                 wrapping.attrs.update({names[1]:lss, names[2]:type})
-                result.append(str(wrapping))
-        # print("result inside function")
-        # print(result)
-        return result
+                res.append(str(wrapping))
+        return res
 
 
 res = ParseFile(root)
-middle = GetTheMiddle()
-mid_res = []
-def Arguments(root):
-    for arguments in root.iter('argument'):
-        arg = arguments.get('argument')
-        themrole = arguments.get('thematicrole')
-        funct = arguments.get('function')
-
-        middle.attrs = {names[3]:arg, names[4]:themrole, names[5]:funct}
-        mid_res.append(str(middle))
-
-    return mid_res
-
-
-# coses = ['casa','cosa']
-mid_res = Arguments(root)
-#
-# for j in range(0, len(mid_res)):
-#     print mid_res[j]
-
-# print mid_res
-
-for j in range(0, len(res)):
-    total  = res[j]
-    for i in range(0, len(mid_res)):
-        total += mid_res[i]
-        total += "\n"
-    print total
-# print "result outside function:"
-# print res[0],"\n", res[1]
-# print str(result)
